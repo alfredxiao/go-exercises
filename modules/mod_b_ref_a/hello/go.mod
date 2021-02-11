4e725d886086df15e1e044ba571b3553aa037a10
Alfred Xiao <alfred.xiao@qantashotels.com>
2021-02-11 21:24:23 +1100

    ref v1.0.0

--- modules/mod_b_ref_a/hello/go.mod
+++ modules/mod_b_ref_a/hello/go.mod
@@ -3,6 +3,7 @@ module hello
 go 1.15
 
 require (
-	github.com/alfredxiao/go-exercises/modules/mod_b_ref_a/greetings v0.0.0-20210211094820-421e61434b3a
+	github.com/alfredxiao/go-exercises v1.0.0 // indirect
+	github.com/alfredxiao/go-exercises/modules/mod_b_ref_a/greetings v0.0.0-20210211095035-cd2598acceb3
 	rsc.io/quote v1.5.2 // indirect
 )
--- modules/mod_b_ref_a/hello/hello.go
+++ modules/mod_b_ref_a/hello/hello.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	g "github.com/alfredxiao/go-exercises/modules/mod_b_ref_a/greetings/greetings"
-	u "github.com/alfredxiao/go-exercises/modules/mod_b_ref_a/greetings/utils"
+	"github.com/alfredxiao/go-exercises/modules/mod_b_ref_a/greetings/greetings"
+	"github.com/alfredxiao/go-exercises/modules/mod_b_ref_a/greetings/utils"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := g.Hello("Gladys")
+	message := greetings.Hello("Gladys")
 	fmt.Println(message)
 
-	fmt.Println(u.Utils())
+	fmt.Println(utils.Utils())
 }
