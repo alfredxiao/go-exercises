a764903098b662e920034d05201651947946838b
Alfred Xiao <alfred.xiao@qantashotels.com>
2021-02-11 23:04:11 +1100

    use multi version of the same module

--- modules/ref_another_mod/go.mod
+++ modules/ref_another_mod/go.mod
@@ -1,9 +1,10 @@
-module testmod
+module usedemomod
 
 go 1.15
 
 require (
-	github.com/alfredxiao/go-demo-module v1.0.1
+	github.com/alfredxiao/go-demo-module v1.1.1
+	github.com/alfredxiao/go-demo-module/v2 v2.0.0
 	golang.org/x/text v0.3.5 // indirect
 	rsc.io/sampler v1.99.99 // indirect
 )
--- modules/ref_another_mod/hello.go
+++ modules/ref_another_mod/hello.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"github.com/alfredxiao/go-demo-module/greetings"
+	v2 "github.com/alfredxiao/go-demo-module/v2/greetings"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	fmt.Println(greetings.Hello("Oldman"))
+	message := v2.Hello("Gladys")
 	fmt.Println(message)
 }
